22ef763997e637d412d744712deb4e48d973ed4a
Beltranax <beltranangel7293@gmail.com>
2023-02-02 02:30:03 +0000

    Lab Completed

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,17 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum= 0;
+
+        int length = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        double average = sum / length;
+
+        return average;
+
     }
 }
